604424b08322c862a6716972d5ba1fa7550e94a9
Piero Genovese <pierogenovese@ppm-jamf-25618.bcocorporate.ad>
2023-08-14 22:44:37 -0300

    adding ios project

---
+++ Memorie/MemorieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorieApp.swift
+//  Memorie
+//
+//  Created by Piero Genovese on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
